0d412112f91eddf14e43829658a8caa5170aac34
kiovchev <iov4ev_kokki@abv.bg>
2019-08-07 20:55:40 +0300

    Add PartParamsViewModel

---
+++ ASP.NET Core/Web/SKAuto.Web.ViewModels/ViewModels/PartViewModels/PartParamsInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SKAuto.Web.ViewModels.ViewModels.PartViewModels
+{
+    public class PartParamsInputModel
+    {
+        public string ModelName { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
